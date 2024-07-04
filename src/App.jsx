074336278f5042a5767e950e8dd064702b5bff32
Joapozzo <pozzojoa@gmail.com>
2024-07-04 14:34:20 -0300

    prueba git

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Mariano Perez</h1>
+      <h1>Mariano Pozzo</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
